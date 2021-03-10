ea21afb7a47c3c4b13b881cf394eee9cb0f5f412
Sam Elamin <hussam.elamin@gmail.com>
2021-03-10 18:04:18 +0000

    update picture name

--- app/services/dialog/avatar_picture_dialog.py
+++ app/services/dialog/avatar_picture_dialog.py
@@ -103,5 +103,5 @@ class AvatarDialog(BaseDialog):
                 return
 
             pic = await combine_frame_and_photo(user_pic)
-            pic = img_to_bio(pic, name='thor_ava.png')
+            pic = img_to_bio(pic, name='my_kylian_avatar.png')
             await message.reply_document(pic, caption=loc.TEXT_AVA_READY, reply_markup=self.menu_kbd())
